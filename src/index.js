dd743adafef24a4a59e44ff84d0c733a442b7ab2
huytag <huytagicloud@gmail.com>
2021-08-21 15:07:22 +0700

    add Router & Controller

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ const handlebars  = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 // Http logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 
@@ -19,14 +26,9 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/view'));
 
 
+//route init
+route(app);
 
-app.get('/', (req, res) => {
-    return res.render('home');
-})
-
-app.get('/news', (req, res) => {
-    return res.render('news');
-})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
