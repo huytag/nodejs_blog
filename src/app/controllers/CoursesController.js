30febac7ddbcf61fbc9f511e1414527b348bd1ce
huytag <huytagicloud@gmail.com>
2021-08-23 08:21:57 +0700

    update code

--- src/app/controllers/CoursesController.js
+++ src/app/controllers/CoursesController.js
@@ -1,7 +1,7 @@
 const Course = require('../models/Course');
 const { mongooseToObject } = require('../../unti/mongo');
 
-class SiteController {
+class CoursesController {
     //([GET] courses/:slug)
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug })
@@ -20,44 +20,39 @@ class SiteController {
 
     //([POST] courses/store)
     store(req, res, next) {
-        const fromData = req.body;
-        fromData.img = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hprT5rIeJebP41R-BFIyeA5fByT27CDX7g&usqp=CAU`;
-        const course = new Course(fromData);
+        req.body.img = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4hprT5rIeJebP41R-BFIyeA5fByT27CDX7g&usqp=CAU`;
+        const course = new Course(req.body);
         course.save()
-            .then(() => res.redirect(`/`))
-            .catch(error => {
-
-            });
+            .then(() => res.redirect(`/me/stored/courses`))
+            .catch(next);
     }
     //([edit] courses/store)
-    edit(req, res, next) {  
+    edit(req, res, next) {
         Course.findById(req.params._id)
             .then(course => res.render('courses/edit', {
-                course: mongooseToObject(course)
+                course: mongooseToObject(course),
             }))
             .catch(next)
     }
     //([PUT] courses/id)
-    update(req, res, next){
-        Course.updateOne({_id: req.params._id}, req.body)
-            .then(() =>  res.redirect('/me/stored/courses'))
+    update(req, res, next) {
+        Course.updateOne({ _id: req.params._id }, req.body)
+            .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
     }
     //([DELETE] courses/id)
-    delete(req, res, next){
+    delete(req, res, next) {
         Course.delete({ _id: req.params._id })
             .then(() => res.redirect('back'))
             .catch(next);
     }
-
-    deleteforce(req, res, next){
+    deleteforce(req, res, next) {
         Course.deleteOne({ _id: req.params._id })
             .then(() => res.redirect('back'))
             .catch(next);
     }
-
-     //([PATH] restore/id)
-    restore(req, res, next){
+    //([DELETE] courses/id)
+    restore(req, res, next) {
         Course.restore({ _id: req.params._id })
             .then(() => res.redirect('back'))
             .catch(next);
@@ -65,4 +60,4 @@ class SiteController {
 
 }
 
-module.exports = new SiteController();
+module.exports = new CoursesController();
--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,15 +1,22 @@
 const Course = require('../models/Course');
 const { mutipleMongooseToObject } = require('../../unti/mongo');
 
+
 class MeController {
     //([GET] courses/create)
     stored(req, res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored', {
-                courses: mutipleMongooseToObject(courses)
-            }))
+        Promise.all([
+            Course.find({}).sortable(req), 
+            Course.countDocumentsDeleted()]
+            )
+            .then(([courses, deleteCount]) =>
+                res.render('me/stored', {
+                    deleteCount,
+                    courses: mutipleMongooseToObject(courses),
+                }))
             .catch(next);
     }
+
     trash(req, res, next) {
         Course.findDeleted({})
             .then(courses => res.render('me/trash', {
